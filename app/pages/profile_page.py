2f6891dc8f0b44bccfff59ca85107bb4cc66598f
M4dhav <madhav.gupta7674@gmail.com>
2025-07-14 23:27:05 +0530

    fix:import

--- app/pages/profile_page.py
+++ app/pages/profile_page.py
@@ -16,6 +16,8 @@ from PyQt6.QtWidgets import (
 from PyQt6.QtGui import QPixmap
 from PyQt6.QtCore import Qt
 from PyQt6.QtGui import QGuiApplication
+from PyQt6.QtCore import pyqtSlot
+
 
 import qrcode
 
